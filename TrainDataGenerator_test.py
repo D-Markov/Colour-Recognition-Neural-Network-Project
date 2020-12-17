87793d9a88e4260b52285f1093735514a388246d
Danny <d.markov@live.co.uk>
2020-12-17 14:57:01 +0000

    Foo function

--- TrainDataGenerator_test.py
+++ TrainDataGenerator_test.py
@@ -7,9 +7,6 @@ class TrainDataGeneratorTest(unittest.TestCase):
         self.image = Image.open("colour_Gradient.jpg")
         self.sutttt = TrainDataGenerator(self.image)
 
-    def foo(self):
-        self.assertEqual(2, len(self.sutttt.image.size))
-
     def test_get_pixel_rgb(self):
         rgb = self.sutttt.get_RGB(36, 415)
         self.assertEqual(rgb, (254, 66, 101))
