b69e3c72118ed4e6cc3b03eb7bd2294102ca0c54
Danny <d.markov@live.co.uk>
2020-12-17 15:18:09 +0000

    Refactor test

--- TrainDataGenerator_test.py
+++ TrainDataGenerator_test.py
@@ -5,10 +5,10 @@ from PIL import Image
 class TrainDataGeneratorTest(unittest.TestCase):
     def setUp(self):
         self.image = Image.open("colour_Gradient.jpg")
-        self.sutttt = TrainDataGenerator(self.image)
+        self.sut = TrainDataGenerator(self.image)
 
     def test_get_pixel_rgb(self):
-        rgb = self.sutttt.get_RGB(36, 415)
+        rgb = self.sut.get_RGB(36, 415)
         self.assertEqual(rgb, (254, 66, 101))
 
     def test_get_colour_name(self):
@@ -16,7 +16,7 @@ class TrainDataGeneratorTest(unittest.TestCase):
         x = [28, 680, 240, 900, 509, 950]
         funcResults = []
         for i in range(len(results)):
-            funcResults.append((self.sutttt.get_colour_name([(40, "red"), (100, "pink"), (300, "blue"), (460, "white"), (520, "light green"), (600, "green"), (840, "yellow"), (910, "orange"), (980, "light red")], x[i])))
+            funcResults.append((self.sut.get_colour_name([(40, "red"), (100, "pink"), (300, "blue"), (460, "white"), (520, "light green"), (600, "green"), (840, "yellow"), (910, "orange"), (980, "light red")], x[i])))
         self.assertListEqual(funcResults, results)
 
 if __name__ == '__main__':
