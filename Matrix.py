34fb5c20e35653b6044a348d8bc6ce691c43a3c0
Danny <d.markov@live.co.uk>
2020-12-25 20:11:25 +0000

    Add factory methods to create zero and random matrices

--- Matrix.py
+++ Matrix.py
@@ -1,74 +1,123 @@
-def add(matA, matB):
-    matC = []
-    for r in range(len(matA)):
-        for c in range(len(matA[r])):
-            matC.append(matA[r][c] + matB[r][c])
+import random
+from multipledispatch import dispatch
+from collections.abc import Sequence
 
-def subtract(matA, matB):
-    matC = []
-    for r in range(len(matA)):
-        for c in range(len(matA[r])):
-            matC.append(matA[r][c] - matB[r][c])
+class Matrix(Sequence):
+    def __init__(self, inputs):
+        if len(inputs) == 0:
+            raise ValueError("Matrix cannot be empty")
 
-
-
-def dot(MatA, MatB):
-    if len(MatA) != len(MatB):
-        return ("Change the dimensions")
-    else:
-        val_sum = 0
-        for i in len(MatA):
-            val_sum += MatA[i] * MatB[i]
-    return val_sum
+        self.rows = len(inputs)
+        self.colomns = len(inputs[0])
+        for i in inputs:
+            if len(i) != self.colomns:
+                raise ValueError("Invalid Dimension")
+        self.__data = [c for c in inputs]
     
 
-# def multiply(MatA, MatB):
-#     matC = []
-#     if len(MatA) != len(MatB[0]):
-#         return ("Invalid dimensions")
-#     elif len(MatB) == 1 and len(MatB[0] == 1):
-#         for r in MatA:
-#             matC.append([r * MatB])
-#     else: 
-#         for i in MatA:
-#             subArr = []
-#             for j in i: 
-#                 subArr.append(MatA[i][j] * MatB[j][i] + Mat)
-#             matC.append(subArr)
-
-def rtocol(Matrix):
-    new_Matrix = []
-    for a in range(len(Matrix)):
-        arr = []
-        for i in a:
-            arr.append(Matrix[a][i])
-        new_Matrix.append(arr)
-
-
-def Multiply(MatA, MatB):
-    if len(MatA) != len(MatB[0]) or len(MatA[0]) != len(MatB):
-        raise ValueError("Invalid dimensions")
+    def __len__(self):
+        return len(self.__data)
     
-    matC = []
 
-    for r in range(len(MatA)):
-        arr = []
-        for c in range(len(MatA[r])):
-            sum = 0
-            for i in range(len(MatA)):
-                sum += MatA[r][i] * MatB[i][c]
-            
-            arr.append(sum)
+    def __getitem__(self, i):
+        return self.__data[i]
         
-        matC.append(arr)
-    
-    return matC
 
-# def T3*3(Matrix):
-#     new_Matrix = []
-#     for i in range(len(Matrix[0])):
-#         arr = []
-#         arr.append([Matrix[i][0], Matrix[i][1], Matrix[i][2]])
-#         new_Matrix.append(arr)
-    
-#     return new_Matrix
+    @staticmethod
+    def zeroMatrix(r, c):
+        newMatrix = []
+        
+        for _ in range(r):
+            newRow = [0 for _ in range(c)]
+            newMatrix.append(newRow)
+        
+        return Matrix(newMatrix)
+
+    @staticmethod
+    def randomMatrix(r, c):
+        newMatrix = []
+        
+        for _ in range(r):
+            newRow = [random.random() for _ in range(c)]
+            newMatrix.append(newRow)
+
+        return Matrix(newMatrix)
+    #range()
+
+
+    def add(self, matB):
+        if self.rows != matB.rows or self.colomns != matB.colomns:
+            raise ValueError("Invalid Dimensions")
+        
+        for r in range(matB.rows):
+            for c in range(len(matB[r])):
+                self.__data[r][c] + matB.__data[r][c]
+
+    def subtract(self, matA, matB):
+        matC = []
+        for r in range(len(matA)):
+        
+            for c in range(len(matA[r])):
+                matC.append(matA[r][c] - matB[r][c])
+
+
+
+    def dot(self, MatA, MatB):
+        if len(MatA) != len(MatB):
+            return ("Change the dimensions")
+        else:
+            val_sum = 0
+            for i in len(MatA):
+                val_sum += MatA[i] * MatB[i]
+        return val_sum
+        
+
+    # def multiply(MatA, MatB):
+    #     matC = []
+    #     if len(MatA) != len(MatB[0]):
+    #         return ("Invalid dimensions")
+    #     elif len(MatB) == 1 and len(MatB[0] == 1):
+    #         for r in MatA:
+    #             matC.append([r * MatB])
+    #     else: 
+    #         for i in MatA:
+    #             subArr = []
+    #             for j in i: 
+    #                 subArr.append(MatA[i][j] * MatB[j][i] + Mat)
+    #             matC.append(subArr)
+
+    def rtocol(self, Matrix):
+        new_Matrix = []
+        for a in range(len(Matrix)):
+            arr = []
+            for i in a:
+                arr.append(Matrix[a][i])
+            new_Matrix.append(arr)
+
+    def multiply(self, MatA, MatB):
+        if len(MatA) != len(MatB[0]) or len(MatA[0]) != len(MatB):
+            raise ValueError("Invalid dimensions")
+        
+        matC = []
+
+        for r in range(len(MatA)):
+            arr = []
+            for c in range(len(MatA[r])):
+                sum = 0
+                for i in range(len(MatA)):
+                    sum += MatA[r][i] * MatB[i][c]
+                
+                arr.append(sum)
+            
+            matC.append(arr)
+        
+        return matC
+
+    # def T3*3(Matrix):
+    #     new_Matrix = []
+    #     for i in range(len(Matrix[0])):
+    #         arr = []
+    #         arr.append([Matrix[i][0], Matrix[i][1], Matrix[i][2]])
+    #         new_Matrix.append(arr)
+        
+    #     return new_Matrix
--- Matrix_test.py
+++ Matrix_test.py
@@ -1,18 +1,34 @@
 import unittest
-import Matrix
+import unittest.mock as mock
+from Matrix import Matrix
+from random import random
 
 class MatrixTest(unittest.TestCase):
 
-    def test_matrix_mathematical_operations(self):
-        multiplication = [[1, 2], [3, 4]]
-        matrixR = Matrix.Multiply([[1, 0], [0, 1]], [[1, 2], [3, 4]])
-        self.assertListEqual(matrixR, multiplication)
+    # def test_matrix_mathematical_operations(self):
+    #     multiplication = [[1, 2], [3, 4]]
+    #     matrixR = Matrix.multiply([[1, 0], [0, 1]], [[1, 2], [3, 4]])
+    #     self.assertListEqual(matrixR, multiplication)
 
-    def test_multiplication_exception_raised_on_invalid_matrix_dimensions_1(self):
-        self.assertRaises(ValueError, Matrix.Multiply, [[1, 0], [0, 1]], [[1, 2]])
+    # def test_multiplication_exception_raised_on_invalid_matrix_dimensions_1(self):
+    #     self.assertRaises(ValueError, Matrix.multiply, [[1, 0], [0, 1]], [[1, 2]])
 
-    def test_multiplication_exception_raised_on_invalid_matrix_dimensions_2(self):
-        self.assertRaises(ValueError, Matrix.Multiply, [[1, 2]], [[1, 0], [0, 1]])
+    # def test_multiplication_exception_raised_on_invalid_matrix_dimensions_2(self):
+    #     self.assertRaises(ValueError, Matrix.multiply, [[1, 2]], [[1, 0], [0, 1]])
+
+    def test_random_matrix(self):
+        with mock.patch("random.random", return_value = 1):
+            randMatrix = Matrix.randomMatrix(2, 4)
+            self.assertSequenceEqual([[1 for c in range(4)], [1 for c in range(4)]], randMatrix)
+
+    def test_zero_matrix(self):
+        zerMatrix = Matrix.zeroMatrix(2, 4)
+        self.assertSequenceEqual([[0, 0, 0, 0],[0, 0, 0, 0]] , zerMatrix)
+
+    # def test_add_matrix(self):
+    #     addMatrix = [[9, 6], [0, 0]]
+    #     matrix = 
+    #     self.assertListEqual(addMatrix, 
 
 if __name__ == '__main__':
     unittest.main()
