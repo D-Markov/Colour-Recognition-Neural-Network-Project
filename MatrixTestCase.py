c1a19aa3b04fa4e4b52825afa9f919d7497c91c4
Danny <d.markov@live.co.uk>
2021-01-29 18:25:57 +0000

    Fix assert message

--- MatrixTestCase.py
+++ MatrixTestCase.py
@@ -6,13 +6,13 @@ class MatrixTestCase(TestCase):
 
     def assertMatrixAreEqual(self, actual: Matrix, expected: Matrix) -> None:
         if(actual.rows != expected.rows or actual.colomns != expected.colomns):
-            raise AssertionError("The Matrix are different")
+            raise AssertionError(f"The Matrix are different\nactual:\n{actual}\nexpected\n{expected}")
 
         rindex = 0
         for row in expected: 
             cindex = 0
             for cell in row:
                 if not math.isclose(cell, actual[rindex][cindex], rel_tol=1e-05):
-                    raise AssertionError("The Matrix are different")
+                    raise AssertionError(f"The Matrix are different\nactual:\n{actual}\nexpected\n{expected}")
                 cindex += 1
             rindex += 1
