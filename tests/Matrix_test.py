01114c49d3e8de1dd165c6c36aaaf6be0287d9d1
Danny <d.markov@live.co.uk>
2021-02-15 15:06:35 +0000

    Fix tests

--- tests/Matrix_test.py
+++ tests/Matrix_test.py
@@ -47,7 +47,7 @@ class MatrixTest(MatrixTestCase):
     def test_random_matrix(self):
         with mock.patch("random.gauss", return_value = 1):
             randMatrix = Matrix.randomMatrix(2, 4)
-            self.assertSequenceEqual([[1 for _ in range(4)], [1 for _ in range(4)]], randMatrix)
+            self.assertSequenceEqual([[0.01 for _ in range(4)], [0.01 for _ in range(4)]], randMatrix)
 
     def test_zero_matrix(self):
         zerMatrix = Matrix.zeroMatrix(2, 4)
