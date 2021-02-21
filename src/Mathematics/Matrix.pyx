04a0848f3dd196463fa3207048b349a7518b6bb3
Danny <d.markov@live.co.uk>
2021-02-21 20:47:49 +0000

    Add static typing

--- src/Mathematics/Matrix.pyx
+++ src/Mathematics/Matrix.pyx
@@ -1,14 +1,27 @@
-from typing import Callable, Union, Sequence, List
+# cython: profile=True
+# cython: linetrace=True
+
+# distutils: define_macros=CYTHON_TRACE=1
+# distutils: language=c++
+# cython: language_level=3
+
+from typing import Callable, Union, List
 import random
 
 Scalar = Union[int, float]
 
-class Matrix(Sequence[List[Scalar]]):
+cdef class Matrix:
+    cdef:
+        Py_ssize_t rows, colomns
+        list __data
+
     def __init__(self, inputs: Union[List[List[int]], List[List[float]]]):
         if len(inputs) == 0:
             raise ValueError("Matrix cannot be empty")
-        self.rows: int = len(inputs)
-        self.colomns: int = len(inputs[0])
+
+        self.rows = len(inputs)
+        self.colomns = len(inputs[0])
+        
         for i in inputs:
             if len(i) != self.colomns:
                 raise ValueError("Invalid Dimension")
@@ -17,7 +30,7 @@ class Matrix(Sequence[List[Scalar]]):
     def __len__(self):
         return len(self.__data)
     
-    def __getitem__(self, i: int):
+    def __getitem__(self, i):
         return self.__data[i]
 
     def __str__(self):
@@ -38,8 +51,16 @@ class Matrix(Sequence[List[Scalar]]):
     def __rsub__(self, other: Union['Matrix', Scalar]) -> 'Matrix':
         return self.__subtract(other, True)     
  
+    @property
+    def rows(self):
+        return self.rows
+ 
+    @property
+    def colomns(self):
+        return self.colomns
+        
     @staticmethod
-    def zeroMatrix(r: int, c: int) -> 'Matrix':
+    def zeroMatrix(r, c) -> 'Matrix':
         newMatrix = []
         
         for _ in range(r):
@@ -49,7 +70,7 @@ class Matrix(Sequence[List[Scalar]]):
         return Matrix(newMatrix)
 
     @staticmethod
-    def randomMatrix(r: int, c: int) -> 'Matrix':
+    def randomMatrix(r, c) -> 'Matrix':
         newMatrix = []
         
         for _ in range(r):
@@ -58,22 +79,29 @@ class Matrix(Sequence[List[Scalar]]):
 
         return Matrix(newMatrix)
  
-
-    def dot(self, MatB: 'Matrix') -> 'Matrix':
+    cpdef Matrix dot(self, Matrix MatB):
         if self.colomns != MatB.rows: #l self.rows != MatB.colomns:
             raise ValueError("Change the dimensions")
-            
-        else:
-            new_data = [[0.0] * MatB.colomns for _ in range(self.rows)]
-            
-            for r in range(self.rows):
-                for c in range(MatB.colomns):
-                    val_sum: Scalar = 0
-                    for idx in range(self.colomns):
-                        val_sum += self.__data[r][idx] * MatB.__data[idx][c]
-                    new_data[r][c] = val_sum
         
-            return Matrix(new_data)
+        cdef:
+            int r, c, idx
+            float val_sum, a, b
+
+        new_data = [[0.0] * MatB.colomns for _ in range(self.rows)]
+        
+        for r in range(self.rows):
+            s_row = self.__data[r]
+            n_row = new_data[r]
+            
+            for c in range(MatB.colomns):
+                val_sum = 0
+                for idx in range(self.colomns):
+                    a = s_row[idx]
+                    b = MatB.__data[idx][c]
+                    val_sum += a * b
+                n_row[c] = val_sum
+    
+        return Matrix(new_data)
 
 #    def dot1(self, MatB):
 #        matrixR = MatB.rtocol()
@@ -113,11 +141,11 @@ class Matrix(Sequence[List[Scalar]]):
         return result
 
     def multiply(self, other: Union[Scalar, 'Matrix']) -> 'Matrix':
-        return Matrix.__broadcast(self, other, lambda a, b: a * b)
+        return self.__broadcast(other, lambda a, b: a * b)
 
 
     def divide(self, other: Union[Scalar, 'Matrix']) -> 'Matrix':
-        return Matrix.__broadcast(self, other, lambda a, b: a / b)
+        return self.__broadcast(other, lambda a, b: a / b)
 
 
     # def __mul__(self, other):
@@ -132,63 +160,65 @@ class Matrix(Sequence[List[Scalar]]):
     # def __rdiv__(self, other):
     #     return Matrix.__broadcast(other, self, lambda a, b: a / b)
 
-    @staticmethod
-    def __broadcast( 
-        left: 'Matrix',
-        right: Union[Scalar, 'Matrix'],
-        operation: Callable[[Scalar, Scalar], Scalar]) -> 'Matrix':
-        
-        if not(type(right) is int or type(right) is float or type(right) is Matrix):
-            raise ValueError("Only int, float or Matrix allowed")
+    def __broadcast(self, other: Union[Matrix, Scalar], operation: Callable[[Scalar, Scalar], Scalar]) -> Matrix:
+        if(isinstance(other, int) or isinstance(other, float)):
+            return self.__broadcast_n(other, operation)
+        elif(isinstance(other, Matrix)):
+            return self.__broadcast_m(other, operation)
+        else:
+            raise TypeError
 
-        if isinstance(right,int) or isinstance(right, float):
+    cpdef Matrix __broadcast_n(self, float right, operation: Callable[[Scalar, Scalar], Scalar]):
+        new_data = []
+
+        for row_idx in range(self.rows):
+            new_row = []
+            for col_idx in range(self.colomns):
+                new_row.append(operation(self.__data[row_idx][col_idx],  right))
+            new_data.append(new_row)
+
+        return Matrix(new_data)
+
+    cpdef Matrix __broadcast_m(self, Matrix right, operation: Callable[[Scalar, Scalar], Scalar]):
+        if self.rows == right.rows and (self.colomns == 1 or right.colomns == 1):
+            colomns =  self.colomns if self.colomns >= right.colomns else right.colomns
+            new_data = [[0.0] * colomns for _ in range(self.rows)]
+            for row_idx in range(self.rows):
+                for col_idx in range(colomns):
+                    left_operand = self[row_idx][0] if self.colomns == 1 else self[row_idx][col_idx]
+                    right_operand = right[row_idx][0] if right.colomns == 1 else right[row_idx][col_idx]
+                    new_data[row_idx][col_idx] = operation(left_operand , right_operand)
+            return Matrix(new_data)
+        elif self.colomns == right.colomns and (self.rows == 1 or right.rows == 1):
+            rows = self.rows if self.rows >= right.rows else right.rows
+            new_data = [[0.0] * self.colomns for _ in range(rows)]
+            for  col_idx in range(self.colomns):
+                for row_idx in range(rows):
+                    left_operand = self[0][col_idx] if self.rows == 1 else self[row_idx][col_idx]
+                    right_operand = right[0][col_idx] if right.rows == 1 else right[row_idx][col_idx]
+                    new_data[row_idx][col_idx] = operation(left_operand, right_operand)
+            return Matrix(new_data)
+        elif self.rows == right.rows and self.colomns == right.colomns:
             new_data = []
-            for row_idx in range(left.rows):
+            for row_idx in range(self.rows):
                 new_row = []
-                for col_idx in range(left.colomns):
-                    new_row.append(operation(left.__data[row_idx][col_idx],  right))
+                for col_idx in range(self.colomns):
+                    new_row.append(operation(self.__data[row_idx][col_idx],  right.__data[row_idx][col_idx]))
                 new_data.append(new_row)
             return Matrix(new_data)
-        else:
-            if left.rows == right.rows and (left.colomns == 1 or right.colomns == 1):
-                colomns =  left.colomns if left.colomns >= right.colomns else right.colomns
-                new_data = [[0.0] * colomns for _ in range(left.rows)]
-                for row_idx in range(left.rows):
-                    for col_idx in range(colomns):
-                        left_operand = left[row_idx][0] if left.colomns == 1 else left[row_idx][col_idx]
-                        right_operand = right[row_idx][0] if right.colomns == 1 else right[row_idx][col_idx]
-                        new_data[row_idx][col_idx] = operation(left_operand , right_operand)
-                return Matrix(new_data)
-            elif left.colomns == right.colomns and (left.rows == 1 or right.rows == 1):
-                rows = left.rows if left.rows >= right.rows else right.rows
-                new_data = [[0.0] * left.colomns for _ in range(rows)]
-                for  col_idx in range(left.colomns):
-                    for row_idx in range(rows):
-                        left_operand = left[0][col_idx] if left.rows == 1 else left[row_idx][col_idx]
-                        right_operand = right[0][col_idx] if right.rows == 1 else right[row_idx][col_idx]
-                        new_data[row_idx][col_idx] = operation(left_operand, right_operand)
-                return Matrix(new_data)
-            elif left.rows == right.rows and left.colomns == right.colomns:
-                new_data = []
-                for row_idx in range(left.rows):
-                    new_row = []
-                    for col_idx in range(left.colomns):
-                        new_row.append(operation(left.__data[row_idx][col_idx],  right.__data[row_idx][col_idx]))
-                    new_data.append(new_row)
-                return Matrix(new_data)
-            elif (left.rows == 1 and right.colomns == 1) or (left.colomns == 1 and right.rows == 1):
-                rows_count = max(left.rows, right.rows)
-                colomn_count = max(left.colomns, right.colomns)
-                new_data = [[0.0] * colomn_count for _ in range(rows_count)]
+        elif (self.rows == 1 and right.colomns == 1) or (self.colomns == 1 and right.rows == 1):
+            rows_count = max(self.rows, right.rows)
+            colomn_count = max(self.colomns, right.colomns)
+            new_data = [[0.0] * colomn_count for _ in range(rows_count)]
 
-                for row_idx in range(rows_count):
-                    for col_idx in range(colomn_count):
-                        left_operand = left[0][col_idx] if left.rows == 1 else left[row_idx][0]
-                        right_operand = right[0][col_idx] if right.rows == 1 else right[col_idx][0]
-                        new_data[row_idx][col_idx] = operation(left_operand, right_operand)
-                return Matrix(new_data)
-            else:
-                raise ValueError("Invalid matrix dimensions")
+            for row_idx in range(rows_count):
+                for col_idx in range(colomn_count):
+                    left_operand = self[0][col_idx] if self.rows == 1 else self[row_idx][0]
+                    right_operand = right[0][col_idx] if right.rows == 1 else right[col_idx][0]
+                    new_data[row_idx][col_idx] = operation(left_operand, right_operand)
+            return Matrix(new_data)
+        else:
+            raise ValueError(f"Invalid matrix dimensions: left ({self.rows}, {self.colomns}); right ({right.rows}, {right.colomns})")
 
 
     def apply(self, func: Callable[[Scalar], Scalar]) -> 'Matrix':
@@ -202,7 +232,7 @@ class Matrix(Sequence[List[Scalar]]):
 
 
     def __add(self, other: Union['Matrix', Scalar]) -> 'Matrix':
-        return Matrix.__broadcast(self, other, lambda a, b: a + b)
+        return self.__broadcast(other, lambda a, b: a + b)
 
     def __subtract(self, other: Union['Matrix', Scalar], isRight: bool = False) -> 'Matrix':
-        return Matrix.__broadcast(self, other, lambda a, b: b - a if isRight else a - b)
+        return self.__broadcast(other, lambda a, b: b - a if isRight else a - b)
