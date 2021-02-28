ef6a73718d0854c125f594519583c998b077a3d1
Danny <d.markov@live.co.uk>
2021-02-28 20:20:33 +0000

    Add module parameters

--- benchmarks/network.py
+++ benchmarks/network.py
@@ -4,7 +4,9 @@ sys.path.append(getcwd())
 
 import cProfile
 import pstats
+import timeit
 
+import argparse
 from pathlib import Path
 from array import array
 from datetime import datetime
@@ -16,9 +18,9 @@ from src.Model.Layer import Layer
 from src.Mathematics.Model_Calculations import cost, error_prime
 from src.Model.Network import Network
 
-def get_output_file_name():
+def get_output_file_name(tag: str) -> str:
     p = Path(__file__)
-    file_name = f'{p.stem}-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}'
+    file_name = f'{p.stem}-{tag}'
     return path.join(p.parent, file_name)
 
 profiler = cProfile.Profile()
@@ -32,18 +34,41 @@ layers = [
 ]
 nn = Network(layers)
 
-profiler.enable()
-for i in range(1):
-    nn.train(imagesM, labels, cost, error_prime, 0.005)
-profiler.disable()
+def doProfile(tag: str) -> None:
+    profiler.enable()
+    for i in range(1):
+        nn.train(imagesM, labels, cost, error_prime, 0.005)
+    profiler.disable()
 
-profile_file_name = get_output_file_name()
-profiler.dump_stats(f"{profile_file_name}.prof")
-with open(f"{profile_file_name}.txt", "w") as f:
-    ps = pstats.Stats(profiler, stream=f).strip_dirs()
-    ps.print_stats()
+    profile_file_name = get_output_file_name(tag)
+    profiler.dump_stats(f"{profile_file_name}.prof")
+    with open(f"{profile_file_name}.txt", "w") as f:
+        ps = pstats.Stats(profiler, stream=f).strip_dirs()
+        ps.print_stats()
 
-profiler.print_stats()
+    profiler.print_stats()
+
+def doTimeit(repeat: int):
+    t = timeit.Timer(lambda: nn.train(imagesM, labels, cost, error_prime, 0.005))
+    res = t.repeat(repeat=repeat, number=1)
+    print(f"Min: {min(res)} Max: {max(res)} Avg: {sum(res)/len(res)}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run Network performance tests.')
+    parser.add_argument('--profile', help="runs cProfile", type=str, nargs='?', const=datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+    parser.add_argument('--timeit', help="runs timeit 10 times", type=int, nargs='?', const=10)
+
+    args = parser.parse_args()
+    if not any([args.profile, args.timeit]):
+        parser.error('No arguments provided.')
+
+    if args.profile:
+        print('Profiling...')
+        doProfile(args.profile)
+    if args.timeit:
+        print(f'Timing {args.timeit} times...')
+        doTimeit(args.timeit)
 
 # To generate html with flame graph: from the project root run:
 # python .\flameprof.py  <name.prof> > <name.html>
