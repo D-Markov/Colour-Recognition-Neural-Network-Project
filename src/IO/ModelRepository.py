e00ce75e10ee3cef5a3f92453629fcd60e2c9442
Danny <d.markov@live.co.uk>
2021-03-03 17:55:53 +0000

    Add persistance of costs

--- src/IO/ModelRepository.py
+++ src/IO/ModelRepository.py
@@ -40,6 +40,12 @@ class ModelRepository:
                 writer = csv.writer(f)
                 for row in layer.biases:
                     writer.writerow(row)
-    
+
+
+    def write_costs(self, costs: List[float]):
+        with open(fr'{self.__folder_name}\costs.csv', 'x') as f:
+            f.write("/n".join([str(x) for x in costs]))
+
+
     def remove(self) -> None:
         rmtree(self.__folder_name)
